cd4d8c7eb4dcea7ad014ca4f28a409fbf2d969b6
tameram <tamer.dweat@gmail.com>
2021-06-08 20:54:49 +0300

    get API weather

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,6 +1,18 @@
 const express = require('express')
 const router = express.Router()
 
-const Expense = require("../model/City")
+const City = require("../model/City")
+const APIKEY = 'c2176abe1c13e806283f16c7681d21de'
+// https://api.openweathermap.org/data/2.5/weather?q=jerusalem&appid=c2176abe1c13e806283f16c7681d21de
+//=jerusalem&appid=c2176abe1c13e806283f16c7681d21de
+const weatherAPI = 'https://api.openweathermap.org/data/2.5/weather?q'
+
+const searchCity  = cityName =>{
+    
+}
+
+router.get('/city/:cityName' , function(req , res){
+
+})
 
 module.exports = router
